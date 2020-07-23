0053e9aa7db9ab508d2651ae9d5665e174d69354
John Chubey <johnchubey@gmail.com>
2020-07-23 15:54:27 -0600

    Add hookrouter to React project to handle pages
    Also, change the title of the document, and fix up the Nav Bar.

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import './Home.scss';
+
+const Home = () => {
+
+  return (
+    <div className="Home">
+      <MatchUps />
+    </div>
+  );
+};
+
+export {Home};
---
+++ src/Components/Players/Players.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import './Players.scss';
+
+const Players = () => {
+  return (
+    <div className="Players">
+      <h1>Players Screen here</h1>
+    </div>
+  );
+};
+
+export {Players};
