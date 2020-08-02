c0022bd39f65fd5e15f9786f81460b590a689eeb
John Chubey <johnchubey@gmail.com>
2020-08-02 14:10:23 -0600

    Stats-9: Add some player stats and minor styling

--- src/Components/Players/Players.js
+++ src/Components/Players/Players.js
@@ -1,10 +1,77 @@
 import React, {useState, useEffect} from 'react';
 import './Players.scss';
+import {BASE_LOCAL_API} from "../../Extras/Constants";
 
 const Players = () => {
+  const [allPlayers, setAllPlayers] = useState(null);
+  useEffect( () => {
+    fetch(BASE_LOCAL_API + '/players')
+      .then(results => results.json())
+      .then(res => {
+        setAllPlayers(res);
+      });
+  }, []);
+
+  function getPlayerTable(){
+    if(allPlayers === null){
+      return <p>Loading...</p>
+    } else {
+      return <div id={'PlayerStatsTable-div'}>
+        <table id={'PlayerStatsTable'}>
+          <tr>
+            <th>Player Name</th>
+            <th>Position</th>
+            <th>Games Played</th>
+            <th>Goals</th>
+            <th>Assists</th>
+            <th>Points</th>
+            <th>+/-</th>
+            <th>Shots</th>
+            <th>Shot %</th>
+            <th>Shifts</th>
+            <th>TOI/G</th>
+            <th>Blocked Shots</th>
+            <th>Hits</th>
+            <th>PIM</th>
+            <th>SHG</th>
+            <th>SHP</th>
+            <th>SHTOI</th>
+            <th>PPG</th>
+            <th>PPP</th>
+            <th>PPTOI</th>
+          </tr>
+          {allPlayers.map(player => {
+            return <tr>
+              <td>{player.player.fullName}</td>
+              <td>{player.position.abbreviation}</td>
+              <td>{player.stats.games}</td>
+              <td>{player.stats.goals}</td>
+              <td>{player.stats.assists}</td>
+              <td>{player.stats.points}</td>
+              <td>{player.stats.plusMinus}</td>
+              <td>{player.stats.shots}</td>
+              <td>{player.stats.shotPct}%</td>
+              <td>{player.stats.shifts}</td>
+              <td>{player.stats.timeOnIcePerGame}</td>
+              <td>{player.stats.blocked}</td>
+              <td>{player.stats.hits}</td>
+              <td>{player.stats.pim}</td>
+              <td>{player.stats.shortHandedGoals}</td>
+              <td>{player.stats.shortHandedPoints}</td>
+              <td>{player.stats.shortHandedTimeOnIcePerGame}</td>
+              <td>{player.stats.powerPlayGoals}</td>
+              <td>{player.stats.powerPlayPoints}</td>
+              <td>{player.stats.powerPlayTimeOnIcePerGame}</td>
+            </tr>
+          })}
+        </table>
+      </div>
+    }
+  }
+
   return (
     <div className="Players">
-      <h1>Players Screen here</h1>
+      {getPlayerTable()}
     </div>
   );
 };
--- src/Extras/Constants.js
+++ src/Extras/Constants.js
@@ -1,6 +1,7 @@
 
 export const BASE_API = 'https://statsapi.web.nhl.com/api/v1/';
 export const BASE_LOGO_API = 'https://www-league.nhlstatic.com/images/logos/teams-current-primary-light/';
+export const BASE_LOCAL_API = 'http://localhost:5000';
 
 export const HOME = 'home';
 export const AWAY = 'away';
