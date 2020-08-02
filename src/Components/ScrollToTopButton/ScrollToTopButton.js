f6d8c43a4a0e1a12a7d3a94e08551158ddbcce12
JohnChubey <johnchubey@gmail.com>
2020-08-02 01:48:57 -0600

    stats-7: Get basic styling finished

--- src/Components/ScrollToTopButton/ScrollToTopButton.js
+++ src/Components/ScrollToTopButton/ScrollToTopButton.js
@@ -2,14 +2,36 @@ import React, {useState, useEffect} from 'react';
 import './ScrollToTopButton.scss';
 
 const ScrollToTopButton = (props) => {
-    const [startDate, setStartDate] = useState(1);
+    const [showButton, setShowButton] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', setButtonState);
+        return () => window.removeEventListener('scroll', setButtonState);
+    })
+
+    function setButtonState(e) {
+        if(window.pageYOffset === 0){
+            setShowButton(false)
+        }
+        else {
+            setShowButton(true)
+        }
+    }
+
+    function getStying(){
+        if(showButton){
+            return {display:null};
+        } else {
+            return {};
+        }
+    }
 
     function scrollToTop() {
         window.scrollTo({top: 0, behavior: "smooth"})
-    };
+    }
 
     return (
-        <button id={'Back-to-top-button'} onClick={scrollToTop}>Back to Top</button>
+        <button id={'Back-to-top-button'} style={getStying()} onClick={scrollToTop}>Back to Top</button>
     );
 }
 
